b2cdb402d6c47ae639a4d9d288d57ce80e82ba7b
zakkudo <zakkudo2@gmail.com>
2015-06-01 08:49:55 -0700

    Added dictionary testing directory

---
+++ src/dictionary/tests/testedictionary.c
@@ -0,0 +1,37 @@
+#include <string.h>
+#include <glib.h>
+#include <glib/gstdio.h>
+#include <libwaei/dictionary.h>
+#include <libwaei/dictionary/edictionary.h>
+
+
+struct _Fixture { LwDictionary * dictionary;};
+typedef struct _Fixture Fixture;
+
+
+void setup (Fixture *fixture, gconstpointer data)
+{
+}
+
+void teardown (Fixture *fixture, gconstpointer data)
+{
+  if (fixture->dictionary != NULL) g_object_unref (fixture->dictionary);
+}
+
+
+void
+test (Fixture * fixture, gconstpointer data)
+{
+  fixture->dictionary = lw_dictionary_new (LW_TYPE_EDICTIONARY, "test");
+}
+
+gint
+main (gint argc, gchar *argv[])
+{
+    g_test_init (&argc, &argv, NULL);
+
+    g_test_add ("/test", Fixture, NULL, setup, test, teardown);
+
+
+    return g_test_run ();
+}
